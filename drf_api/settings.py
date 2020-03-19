454c71a4482fc6743e5a9f520c36f0f76ad7a0d8
Soknoy <47591457+Soknoy@users.noreply.github.com>
2020-03-20 03:14:59 +0700

    connect with db(postgresql)

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'drf_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'drf_api',
+        'USER': 'postgres',
+        'PASSWORD': 'Soknoy010900479',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
